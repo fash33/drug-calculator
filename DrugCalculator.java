dc052461877b4375910002f8c1b0f16855cc34dc
fash33 <farhan.hamid39@gmail.com>
2015-08-06 19:54:12 +0000

    changed class and initialized tester

--- DrugCalculator.java
+++ DrugCalculator.java
@@ -2,16 +2,17 @@
 import java.util.Date;
 public class DrugCalculator {
     
-        private static String[] drugs = {"Angilock","Bipinor","Lithusun SR","Esita","Clonium","Omega 3"};
+        private String[] drugs = {"Angilock","Bipinor","Lithusun SR","Esita","Clonium","Omega 3"};
     
+        public String[] getDrugs() {
+            return drugs;
+            
+        }
     
-    
-    public static void printDrugs() {
+    public static void printDrugs(String[] drugs) {
         for (int i=0;i<drugs.length;i++) {
             System.out.println(drugs[i]);
         }
     }
-    public static void main(String[] args){
-        printDrugs();
-        }
+   
 }
