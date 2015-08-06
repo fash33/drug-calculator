e023d0c1f54513f6cbaf9f98befaf8f9ee1a82b5
fash33 <farhan.hamid39@gmail.com>
2015-08-06 19:50:27 +0000

    made changes to main class

--- DrugCalculator.java
+++ DrugCalculator.java
@@ -1,14 +1,17 @@
 
 import java.util.Date;
 public class DrugCalculator {
-    private String[] drugs = {"Angilock","Bipinor","Lithusun SR","Esita","Clonium","Omega 3"};
-    public String[] getDrugs() {
-        return drugs;
-    }
-    public void printDrugs(String[] drugs) {
+    
+        private static String[] drugs = {"Angilock","Bipinor","Lithusun SR","Esita","Clonium","Omega 3"};
+    
+    
+    
+    public static void printDrugs() {
         for (int i=0;i<drugs.length;i++) {
             System.out.println(drugs[i]);
         }
     }
-    
+    public static void main(String[] args){
+        printDrugs();
+        }
 }
